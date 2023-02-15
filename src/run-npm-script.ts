41ccb533c552f4bbef57c27154cc1acb35aeadfc
archyInk <archybool@gmail.com>
2023-02-15 20:05:16 +0800

    🚧 chore(ink-bumpp): release v8.2.11

--- src/run-npm-script.ts
+++ src/run-npm-script.ts
@@ -13,7 +13,7 @@ export async function runNpmScript(script: NpmScript, operation: Operation): Pro
   const { cwd, ignoreScripts } = operation.options
   if (!ignoreScripts) {
     const { data: manifest } = await readJsonFile('package.json', cwd)
-    console.log(isManifest(manifest));
+    console.log(hasScript(manifest, script));
     if (isManifest(manifest) && hasScript(manifest, script)) {
       await ezSpawn.async('npm', ['run', script, '--silent'], { stdio: 'inherit' })
       operation.update({ event: ProgressEvent.NpmScript, script })
